67670ebefbcaee4987ea401e64d7e021ef76dde3
Hyunho NOH <hyunhodl@gmail.com>
2020-06-29 08:32:33 +0900

    create useForm hook

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -6,39 +6,13 @@ import {
     VALIDATOR_REQUIRE,
     VALIDATOR_MINLENGTH,
 } from "../../shared/util/validators";
+import useForm from "../../shared/hooks/form-hook";
 
 import "./NewPlace.css";
 
-const formReducer = (state, action) => {
-    switch (action.type) {
-        case "INPUT_CHANGE":
-            let formIsValid = true;
-            for (const inputId in state.inputs) {
-                if (inputId === action.inputId) {
-                    formIsValid = formIsValid && action.isValid;
-                } else {
-                    formIsValid = formIsValid && state.inputs[inputId].isValid;
-                }
-            }
-            return {
-                ...state,
-                inputs: {
-                    ...state.inputs,
-                    [action.inputId]: {
-                        value: action.value,
-                        isValid: action.isValid,
-                    },
-                },
-                isValid: formIsValid,
-            };
-        default:
-            return state;
-    }
-};
-
 const NewPlace = (props) => {
-    const [formState, dispatch] = useReducer(formReducer, {
-        inputs: {
+    const [formState, inputHandler] = useForm(
+        {
             title: {
                 value: "",
                 isValid: false,
@@ -52,17 +26,8 @@ const NewPlace = (props) => {
                 isValid: false,
             },
         },
-        isValid: false,
-    });
-
-    const inputHandler = useCallback((id, value, isValid) => {
-        dispatch({
-            type: "INPUT_CHANGE",
-            value: value,
-            inputId: id,
-            isValid: isValid,
-        });
-    }, []);
+        false
+    );
 
     const placeSubmitHandler = (event) => {
         event.preventDefault();
