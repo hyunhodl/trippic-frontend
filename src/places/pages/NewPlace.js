21ceb7cbed98569f3c75f041ff5a3e655e3af8cb
Hyunho NOH <hyunhodl@gmail.com>
2020-06-28 20:48:34 +0900

    create input component and newplace page

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,52 @@
 import React from "react";
 
-const NewPlace = () => {
-    return <div>NewPlace Page</div>;
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+    VALIDATOR_REQUIRE,
+    VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+
+import "./NewPlace.css";
+
+const NewPlace = (props) => {
+    const placeSubmitHandler = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <form className="place-form">
+            <Input
+                element="input"
+                type="text"
+                id="title"
+                label="제목"
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="제목을 입력하세요"
+            />
+            <Input
+                element="textarea"
+                id="descriptoin"
+                label="내용"
+                validators={[VALIDATOR_MINLENGTH(5)]}
+                errorText="내용을 입력하세요. (최소 5자 이상)"
+            />
+            <Input
+                element="input"
+                id="address"
+                label="주소"
+                validators={[VALIDATOR_MINLENGTH(5)]}
+                errorText="주소를 입력하세요 (최소 5자 이상)"
+            />
+            <Button
+                type="submit"
+                onSubmit={placeSubmitHandler}
+                disabled={false}
+            >
+                여행지 추가
+            </Button>
+        </form>
+    );
 };
 
 export default NewPlace;
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+import { validate } from "../../util/validators";
 
 import "./Input.css";
 
+const inputReducer = (state, action) => {
+    switch (action.type) {
+        case "CHANGE":
+            return {
+                value: action.value,
+                isValid: validate(action.value, action.validators),
+            };
+        default:
+            return state;
+    }
+};
+
 const Input = (props) => {
-    return <div></div>;
+    const [inputState, dispatch] = useReducer(inputReducer, {
+        value: "",
+        isValid: false,
+    });
+
+    const inputChangeHandler = (event) => {
+        dispatch({
+            type: "CHANGE",
+            value: event.target.value,
+            validators: props.validators,
+        });
+    };
+
+    const element =
+        props.element === "input" ? (
+            <input
+                id={props.id}
+                type={props.type}
+                value={inputState.value}
+                onChange={inputChangeHandler}
+                placeholder={props.placeholder}
+            />
+        ) : (
+            <textarea
+                id={props.id}
+                rows={props.rows || 3}
+                value={inputState.value}
+                onChange={inputChangeHandler}
+            ></textarea>
+        );
+
+    return (
+        <div
+            className={`form-control ${
+                !inputState.isValid && "form-control--invalid"
+            }`}
+        >
+            <label htmlFor={props.id}>{props.label}</label>
+            {element}
+            {!inputState.isValid && <p>{props.errorText}</p>}
+        </div>
+    );
 };
 
 export default Input;
