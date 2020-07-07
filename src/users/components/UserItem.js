da2eb61c5f61a1c05fffb6ccd7518faab0aec34f
Hyunho NOH <hyunhodl@gmail.com>
2020-07-08 04:00:26 +0900

    connect users to images

--- src/users/components/UserItem.js
+++ src/users/components/UserItem.js
@@ -12,7 +12,10 @@ const UserItem = (props) => {
             <Card className="user-item__content">
                 <Link to={`/${props.id}/places`}>
                     <div className="user-item__image">
-                        <Avatar imageUrl={props.imageUrl} alt={props.alt} />
+                        <Avatar
+                            imageUrl={`http://localhost:5000/${props.imageUrl}`}
+                            alt={props.alt}
+                        />
                     </div>
                     <div className="user-item__info">
                         <h2>{props.name}</h2>
