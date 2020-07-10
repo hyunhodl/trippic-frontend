ba61f4a2b0956960b58a91945eaa32ffeb34c539
Hyunho NOH <hyunhodl@gmail.com>
2020-07-10 15:29:12 +0900

    fix minor issues

--- src/places/components/PlacesList.js
+++ src/places/components/PlacesList.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+
 import PlaceItem from "./PlaceItem";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 
+import { AuthContext } from "../../shared/contexts/auth-context";
+
 import "./PlacesList.css";
 
 const PlacesList = (props) => {
+    const auth = useContext(AuthContext);
+    const userId = useParams().userId;
+
     if (props.items.length === 0) {
         return (
             <div className="places-list center">
                 <Card>
-                    <h2>표시할 장소가 없습니다. 새로 등록하시겠습니까?</h2>
-                    <Button to="/places/new">여행지 공유하기</Button>
+                    <h2>표시할 장소가 없습니다.</h2>
+                    {userId === auth.userId ? (
+                        <Button to="/places/new">여행지 공유하기</Button>
+                    ) : (
+                        <Button to="/">돌아가기</Button>
+                    )}
                 </Card>
             </div>
         );
